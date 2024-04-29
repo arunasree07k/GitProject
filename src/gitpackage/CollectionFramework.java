90d8f1e6356a6e190a0205d9b06fa5ca4a3f76e6
aruna <arunasree07k@gmail.com>
2024-04-29 14:37:49 +0530

    first commit

---
+++ src/gitpackage/CollectionFramework.java
@@ -0,0 +1,20 @@
+package gitpackage;
+
+import java.util.ArrayList;
+
+public class CollectionFramework {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(16);
+		al.add(7);
+		al.add(29);
+		al.add(1);
+		al.add(21);
+		al.add(11);
+		al.add(5);
+		al.add(10);
+		System.out.println(al);
+	}
+
+}
